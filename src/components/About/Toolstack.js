177cb6a080524b98904b2382b8b3c5e37949a795
Mahir09 <mahirshaikh932002@gmail.com>
2025-03-31 10:56:40 -0400

    Removed Unnessecary code

--- src/components/About/Toolstack.js
+++ src/components/About/Toolstack.js
@@ -1,39 +1,3 @@
-// import React from "react";
-// import { Col, Row } from "react-bootstrap";
-// import {
-//   SiAndroidstudio,
-//   SiVisualstudiocode,
-//   SiPostman,
-//   SiGithub,
-//   SiUbuntu,
-// } from "react-icons/si";
-
-
-// function Toolstack() {
-//   return (
-//     <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
-//       <Col xs={4} md={2} className="tech-icons">
-//         <SiVisualstudiocode />
-//       </Col>
-//       <Col xs={4} md={2} className="tech-icons">
-//         <SiAndroidstudio />
-//       </Col>      
-//       <Col xs={4} md={2} className="tech-icons">
-//         <SiPostman />
-//       </Col>
-//       <Col xs={4} md={2} className="tech-icons">
-//         <SiGithub />
-//       </Col>      
-//       <Col xs={4} md={2} className="tech-icons">
-//         <SiUbuntu />
-//       </Col>      
-//     </Row>
-//   );
-// }
-
-// export default Toolstack;
-
-
 import React from "react";
 import { Col, Row } from "react-bootstrap";
 import {
